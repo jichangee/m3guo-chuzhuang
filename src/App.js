fcd46cdd752b760a02a8a1e770f690f7af1e394c
moxuy <jichangee@gmail.com>
2022-07-03 17:13:11 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import heroList from "./lib/list";
+import "./App.css";
+import { Space, Input, Button, Table, Card } from "antd";
+
+const columns = [
+  {
+    title: "英雄",
+    key: "hero_name",
+    dataIndex: "hero_name",
+    width: 200,
+    render: (text, record) => (
+      <>
+        <img className="hero-icon" src={record.hero_icon} />
+        <span>{text}</span>
+      </>
+    ),
+  },
+  {
+    title: "出装",
+    key: "equip",
+    dataIndex: "equip",
+    render: (text) => <img className="equip" src={text} />,
+  },
+];
 
 function App() {
+  const [list, setList] = useState([]);
+
+  const search = (q) => {
+    return heroList.filter((item) => item.hero_name.indexOf(q) > -1);
+  };
+
+  const handleSearch = (keyword) => {
+    if (!keyword) {
+      setList([])
+      return
+    }
+    const searchList = search(keyword);
+    setList(searchList);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Space direction="vertical" className="main">
+        <Card>
+          <Space>
+            <Input.Search
+              allowClear
+              placeholder="输入英雄名"
+              onSearch={handleSearch}
+            />
+          </Space>
+        </Card>
+        <Card>
+          <Table pagination={false} columns={columns} dataSource={list} />
+        </Card>
+      </Space>
     </div>
   );
 }
